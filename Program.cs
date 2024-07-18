9d98e6d4f1511f10ab0802f1365ab6f8080872a2
Samuel Jones <samuel.jones@ucr.ac.cr>
2024-07-18 12:21:27 -0600

    Complete Day 1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 public class Program
@@ -12,12 +13,15 @@ public class Program
         int resultTotal = 0;
         int position = 1;
         string resultString = "";
+        //Diccionario para los numeros escritos en ingles y su respectivo valor
+        
         foreach (string word in GetListWords())
         {//Recorre la lista de palabras
             int firstNumber = 0;
             int lastNumber = 0;
 
             Dictionary<int, int> NumeralDigits = GetNumbersInDigits(word);
+            NumeralDigits = GetNumbersOfWords(NumeralDigits, word);
             firstNumber = NumeralDigits.Values.First();
             lastNumber = NumeralDigits.Values.Last();
 
@@ -33,34 +37,51 @@ public class Program
         Console.WriteLine($"Resultado total: {resultTotal}");
         return;
     }
-    
-    public static string GetWordWithExtra(string word, char letter)
+    public static Dictionary<int, int> GetNumbersOfWords(Dictionary<int, int> numeralDigits, string word)
     {
-        string wordWithExtra = "";
-        //Excepcion para la palabra "three" y "eight"
-        for (int i = 0; i < word.Length; i++)
+        int positionWord = 0;
+        int concurrences = 0;
+        int startPosition = 0;
+        Dictionary<int, string> numbers = new Dictionary<int, string>()
         {
-            if (i - 2 > 0 && word[i - 2] != letter && word[i - 1] == letter && word[i] != letter && i < word.Length - 1)
+            {1,"one"},
+            {2,"two"},
+            {3,"three"},
+            {4,"four"},
+            {5,"five"},
+            {6,"six"},
+            {7,"seven"},
+            {8,"eight"},
+            {9,"nine"}
+        };
+
+        foreach (var item in numbers)
+        {
+            //Si el numero esta contenido en la palabra, entonces se agrega al diccionario de respuesta.
+            if (word.Contains(item.Value))
             {
-                if (word[i - 1] == letter && word[i - 2] != letter)
-                {//Si la letra anterior es la que buscamos y la anterior a la anterior no es la que buscamos, entonces
-                    //agregamos la letra que buscamos antes de la letra actual e intentamos con la expresion regular si hay un match
-                    //Si no hay match, entonces eliminamos la letra que agregamos y seguimos con la siguiente
-                    wordWithExtra += letter;
-                    wordWithExtra += word[i];
-                }
-                else
+                //debemos contar el numero de veces que el numero esta en la palabra
+                string wordTemp = word;
+                while(wordTemp.Contains(item.Value))
                 {
-                    wordWithExtra += word[i];
+                    positionWord = wordTemp.IndexOf(item.Value);
+                    numeralDigits.Add(positionWord, item.Key);
+                    //Quiero cambiar la palabra por -
+                    StringBuilder sb = new StringBuilder(wordTemp);
+                    for (int i = 0; i < item.Value.Length; i++)
+                    {
+                        sb[positionWord + i] = '-';
+                    }
+                    wordTemp = sb.ToString();
                 }
+                
             }
-            else
-            {
-                wordWithExtra += word[i];
-            }
+            
         }
-        return wordWithExtra;
+
+        return numeralDigits.OrderBy(m => m.Key).ToDictionary(pair => pair.Key, pair => pair.Value);
     }
+    
     public static Dictionary<int, int> GetNumbersInDigits(string word)
     {
         char[] arrayChar = word.ToCharArray();
@@ -129,5 +150,33 @@ public class Program
         }
         matches = matches.OrderBy(m => m.Index).ToList();
         return matches;
+    }
+
+    public static string GetWordWithExtra(string word, char letter)
+    {
+        string wordWithExtra = "";
+        //Excepcion para la palabra "three" y "eight"
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (i - 2 > 0 && word[i - 2] != letter && word[i - 1] == letter && word[i] != letter && i < word.Length - 1)
+            {
+                if (word[i - 1] == letter && word[i - 2] != letter)
+                {//Si la letra anterior es la que buscamos y la anterior a la anterior no es la que buscamos, entonces
+                    //agregamos la letra que buscamos antes de la letra actual e intentamos con la expresion regular si hay un match
+                    //Si no hay match, entonces eliminamos la letra que agregamos y seguimos con la siguiente
+                    wordWithExtra += letter;
+                    wordWithExtra += word[i];
+                }
+                else
+                {
+                    wordWithExtra += word[i];
+                }
+            }
+            else
+            {
+                wordWithExtra += word[i];
+            }
+        }
+        return wordWithExtra;
     }
 }
